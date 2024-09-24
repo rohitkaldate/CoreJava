4d9f5005f5d648a783da26fd10759594e93fdfa4
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-09-24 20:21:46 +0530

    Constructor and it's types

--- ConstructorConcept.java
+++ ConstructorConcept.java
@@ -9,22 +9,55 @@ public class ConstructorConcept {       //Class name
 
     // }
 
+    // int id;
+    // String name;
+    // ConstructorConcept(int i,String n)  //Parameterized constructor
+    // {
+    //     id=i;
+    //     name=n;
+    // }
+    // void dispaly()
+    // {
+    //     System.out.println(id+" "+name);
+    // }
+    // public static void main(String args[])
+    // {
+    //     ConstructorConcept c1= new ConstructorConcept(11, "Rohit");//Assigning values to parameters
+    //     ConstructorConcept c2=new ConstructorConcept(12,"Rahi");//Assigning values to parameters
+    //     c1.dispaly();
+    //     c2.dispaly();
+    // }
+
+    //Constructor Overloading
+
     int id;
     String name;
-    ConstructorConcept(int i,String n)  //Parameterized constructor
+    int mob;
+
+    ConstructorConcept(int i,String n)
     {
         id=i;
         name=n;
     }
+    ConstructorConcept(int i, String n, int m)
+    {
+        id=i;
+        name=n;
+        mob=m;
+
+    }
+
     void dispaly()
     {
-        System.out.println(id+" "+name);
+        System.out.println(id+" "+name+" "+mob);
     }
     public static void main(String args[])
     {
-        ConstructorConcept c1= new ConstructorConcept(11, "Rohit");//Assigning values to parameters
-        ConstructorConcept c2=new ConstructorConcept(12,"Rahi");//Assigning values to parameters
+        ConstructorConcept c1=new ConstructorConcept(12,"Rohit");
+        ConstructorConcept c2= new ConstructorConcept(12,"Hem",764567567);
         c1.dispaly();
         c2.dispaly();
     }
+
+
 }

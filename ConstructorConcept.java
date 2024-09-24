495987a6782f8bdfaacb0fa15c148d0b3da75321
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-09-24 19:57:54 +0530

    Constructor Concepts

---
+++ ConstructorConcept.java
@@ -0,0 +1,11 @@
+public class ConstructorConcept {
+    ConstructorConcept()
+    {
+        System.out.println("This is Defaukt constructor");
+    }
+    public static void main(String args[])
+    {
+        ConstructorConcept c=new ConstructorConcept();
+        
+    }
+}

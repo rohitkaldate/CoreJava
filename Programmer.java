b6db33d025b995073f425afb66fd04a740e37a0a
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-09-24 20:14:48 +0530

    Single level Inheritance

--- Programmer.java
+++ Programmer.java
@@ -1,17 +1,19 @@
 import java.lang.*;
 
-class Employee
+class Employer
 {
+	String name="Rohit";
 	float salary=40000f;
 }
 
-class Programmer extends Employee
+class Programmer extends Employer
 {
 	int bonus =30000;
 	
 	public static void main(String args[])
 	{
 		Programmer p=new Programmer();
+		System.out.println("Name of employee is: "+p.name);
 		System.out.println("Salary is: "+p.salary);
 		System.out.println("Bonus  is: "+p.bonus);
 	}

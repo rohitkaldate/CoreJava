50ffcdc2f67bf0c2766fce79d5dda18147b402f2
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-09-24 19:28:59 +0530

    Static Method Example

---
+++ StaticMethod.java
@@ -0,0 +1,12 @@
+public class StaticMethod {
+    public static void main(String args[])
+    {
+        show();
+
+    }
+    static void show()
+    {
+        System.out.println("This is static method example");
+    }
+    
+}

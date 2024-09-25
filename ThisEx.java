5aa2c959a4acf67628b9f7287b5763ef97b3c012
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-09-25 19:18:27 +0530

    It is ex of this keyword

--- ThisEx.java
+++ ThisEx.java
@@ -1,26 +1,44 @@
 //Reffer to class instance variable using this keyword
 
-public class ThisEx {
-    int id;
-    String name;
-    float fees;
-    ThisEx(int id, String name, float fees)
+ public class ThisEx {
+//     int id;
+//     String name;
+//     float fees;
+//     ThisEx(int id, String name, float fees)
+//     {
+//         this.id=id;
+//         this.name=name;
+//         this.fees=fees;
+//     }
+//     void dispaly()
+//     {
+//         System.out.println(id+" "+name+" "+fees);
+//     }
+
+//     public static void main (String args[])
+//     {
+//         ThisEx t=new ThisEx(1, "Rushi", 45761);
+//         ThisEx t1=new ThisEx(123,"Raj" ,58767);
+//         t.dispaly();
+//         t1.dispaly();
+//     }
+
+//This keyword to invoke method in the another method using the this keyword:
+    void function()
     {
-        this.id=id;
-        this.name=name;
-        this.fees=fees;
+        System.out.println("Say hii");
     }
     void dispaly()
     {
-        System.out.println(id+" "+name+" "+fees);
-    }
+        System.out.println("Say welcome");
+        this.function();
 
-    public static void main (String args[])
+    }
+    public static void main(String args[])
     {
-        ThisEx t=new ThisEx(1, "Rushi", 45761);
-        ThisEx t1=new ThisEx(123,"Raj" ,58767);
-        t.dispaly();
+        ThisEx t1=new ThisEx();
         t1.dispaly();
+        
     }
-
 }
+

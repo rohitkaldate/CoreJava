59df21cab22c6cf378b2d69477604d53c628bcab
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-10-06 14:36:25 +0530

    Example of exception handling

--- Exception.java
+++ Exception.java
@@ -1,13 +1,29 @@
 //Example of exception handling 
-public class Exception {
+// public class Exception {
+//     public static void main(String[] args) {
+//         try{
+//             int a = 100/0;  // ArithmeticException
+//         }
+//         catch(ArithmeticException e)
+//         {
+//             System.out.println(e);  // printing the arithmetic exception 
+//         }
+//         System.out.println("Runs the rest of code"); //Printing the rest of code out of the exception.
+//     }
+//}
+
+//Handle exception using the parent class exception
+
+class Exception{
     public static void main(String[] args) {
         try{
-            int a = 100/0;  // ArithmeticException
+            int i=20/0;
+            System.out.println("Run code");
         }
         catch(ArithmeticException e)
         {
-            System.out.println(e);  // printing the arithmetic exception 
+            System.out.println("can't divide by 0 ");
         }
-        System.out.println("Runs the rest of code"); //Printing the rest of code out of the exception.
+      
     }
 }
